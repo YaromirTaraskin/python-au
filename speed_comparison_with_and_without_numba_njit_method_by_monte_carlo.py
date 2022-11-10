0428054bf2501ba62bd22ab2967c326d023af69d
Yaromir Taraskin <62649776+YaromirTaraskin@users.noreply.github.com>
2022-11-11 00:01:43 +0300

    Added speed comparison: method_by_monte_carlo vs method_by_monte_carlo_numba_jit

---
+++ speed_comparison_with_and_without_numba_njit_method_by_monte_carlo.py
@@ -0,0 +1,25 @@
+import method_by_monte_carlo
+import method_by_monte_carlo_numba_jit
+
+
+def print_divider():
+    print()
+    print("---")
+    print()
+
+
+def main():
+    data = method_by_monte_carlo.enter_data()
+    print_divider()
+    print("Calculating without numba njit...")
+    print()
+    method_by_monte_carlo.perform_calculations(*data)
+    print_divider()
+    print("Calculating with numba njit...")
+    print()
+    method_by_monte_carlo_numba_jit.perform_calculations(*data)
+    print_divider()
+
+
+if __name__ == "__main__":
+    main()
