113ce7b11c2e25dc4fdca2ac82bf94d27dbb40b5
Yaromir Taraskin <62649776+YaromirTaraskin@users.noreply.github.com>
2023-04-05 05:11:22 +0300

    Added Flask Game-of-Live project

--- flask_app/game_of_life/app.py
+++ flask_app/game_of_life/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from game_of_life import GameOfLife
 
 
 app = Flask(__name__)
@@ -6,7 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    GameOfLife(width=20, height=18)
     return render_template('index.html')
+
+
+@app.route('/live')
+def live():
+    running_game_of_live = GameOfLife()
+    running_game_of_live.form_new_generation()
+    return render_template('live.html', running_game_of_live=running_game_of_live)
 
 
 if __name__ == '__main__':
